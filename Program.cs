6805bb2382b2d32df8e85958d0c33beb9d48ddc6
dynamics <31436988+dynamics1@users.noreply.github.com>
2022-12-01 15:07:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+
+
+	{
+		Console.Write("Introduceti cate numere va contine sirul (de la 1 la n)");
+		Console.Write("\n");
+		Console.Write("n= ");
+		int n;
+		int s = 1;
+		int p = 1;
+		n = Convert.ToInt32(Console.ReadLine());
+
+		for (int i = 1; i <= n; i++)
+
+		{
+			p *= i;
+			s += i;
+		}
+		Console.Write("Suma numerelor de la 1 la "+ n + " = " + s + "\n");
+		Console.Write("Produsul numerelor de la 1 la " + n + " = " + p + "\n");
+
+	}
+}
